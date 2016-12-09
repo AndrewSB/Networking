06bb422c66059469ef13176af0854da44318e484
Andrew Breckenridge <asbreckenridge@me.com>
2016-12-08 18:20:35 -0800

    implement github.zen demo

--- Demo/AppDelegate.swift
+++ Demo/AppDelegate.swift
@@ -1,13 +1,23 @@
 import UIKit
+import Networking
+import RxSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    private let disposeBag = DisposeBag()
 
     var window: UIWindow?
 
     let networking = Networking()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        networking.request(.zen)
+            .subscribe(onNext: { response in
+                print("response recieved from zen!")
+                print(String(data: response.data, encoding: .utf8)!)
+            })
+            .addDisposableTo(disposeBag)
 
         return true
     }
